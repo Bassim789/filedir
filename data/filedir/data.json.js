4ced47231865924fc4bf08565fd7aedf3f9bc75a
Bassim <bassim.matar@gmail.com>
2020-05-10 17:30:28 +0200

    dont ignore data.json.js

---
+++ data/filedir/data.json.js
@@ -0,0 +1,978 @@
+data = 
+{
+ "files_data": [
+  {
+   "path": "", 
+   "last_modif": 1589113634.0, 
+   "size": 2150, 
+   "file_name": "index.html"
+  }, 
+  {
+   "path": "", 
+   "last_modif": 1589122962.0, 
+   "size": 10244, 
+   "file_name": ".DS_Store"
+  }, 
+  {
+   "path": "", 
+   "last_modif": 1583002802.0, 
+   "size": 35129, 
+   "file_name": "LICENSE"
+  }, 
+  {
+   "path": "", 
+   "last_modif": 1589124142.0, 
+   "size": 33, 
+   "file_name": "catalog_names.json.js"
+  }, 
+  {
+   "path": "", 
+   "last_modif": 1588512751.0, 
+   "size": 2126, 
+   "file_name": "README.md"
+  }, 
+  {
+   "path": "", 
+   "last_modif": 1589124272.0, 
+   "size": 87, 
+   "file_name": "scan.command"
+  }, 
+  {
+   "path": "", 
+   "last_modif": 1589029449.0, 
+   "size": 44, 
+   "file_name": ".gitignore"
+  }, 
+  {
+   "path": "", 
+   "last_modif": 1583002802.0, 
+   "size": 66, 
+   "file_name": ".gitattributes"
+  }, 
+  {
+   "path": "", 
+   "last_modif": 1589124267.0, 
+   "size": 5615, 
+   "file_name": "scan.py"
+  }, 
+  {
+   "path": "", 
+   "last_modif": 1588512092.0, 
+   "size": 3782, 
+   "file_name": "scan.R"
+  }, 
+  {
+   "path": "", 
+   "last_modif": 1588805580.0, 
+   "size": 6278, 
+   "file_name": "scan.cs"
+  }, 
+  {
+   "path": "config", 
+   "last_modif": 1589124306.0, 
+   "size": 81, 
+   "file_name": "filedir.txt"
+  }, 
+  {
+   "path": "web", 
+   "last_modif": 1589122897.0, 
+   "size": 10244, 
+   "file_name": ".DS_Store"
+  }, 
+  {
+   "path": "web", 
+   "last_modif": 1584460341.0, 
+   "size": 839, 
+   "file_name": "main.css"
+  }, 
+  {
+   "path": "web", 
+   "last_modif": 1589116036.0, 
+   "size": 1194, 
+   "file_name": "app.js"
+  }, 
+  {
+   "path": "web/module", 
+   "last_modif": 1582992797.0, 
+   "size": 1195, 
+   "file_name": "Url_params.js"
+  }, 
+  {
+   "path": "web/module", 
+   "last_modif": 1583797396.0, 
+   "size": 587, 
+   "file_name": "utils.js"
+  }, 
+  {
+   "path": "web/module", 
+   "last_modif": 1589112130.0, 
+   "size": 770, 
+   "file_name": "Loader.js"
+  }, 
+  {
+   "path": "web/module/footer", 
+   "last_modif": 1584234517.0, 
+   "size": 204, 
+   "file_name": "footer.html.js"
+  }, 
+  {
+   "path": "web/module/footer", 
+   "last_modif": 1584460293.0, 
+   "size": 157, 
+   "file_name": "footer.css"
+  }, 
+  {
+   "path": "web/module/footer", 
+   "last_modif": 1584234511.0, 
+   "size": 293, 
+   "file_name": "footer.js"
+  }, 
+  {
+   "path": "web/module/dark_mode", 
+   "last_modif": 1575932195.0, 
+   "size": 163, 
+   "file_name": "dark_mode.html.js"
+  }, 
+  {
+   "path": "web/module/dark_mode", 
+   "last_modif": 1588982707.0, 
+   "size": 1592, 
+   "file_name": "dark_mode.css"
+  }, 
+  {
+   "path": "web/module/dark_mode", 
+   "last_modif": 1584114178.0, 
+   "size": 975, 
+   "file_name": "dark_mode_slider.css"
+  }, 
+  {
+   "path": "web/module/dark_mode", 
+   "last_modif": 1577384635.0, 
+   "size": 876, 
+   "file_name": "dark_mode.js"
+  }, 
+  {
+   "path": "web/module/filedir", 
+   "last_modif": 1589123251.0, 
+   "size": 7847, 
+   "file_name": "filedir.css"
+  }, 
+  {
+   "path": "web/module/filedir", 
+   "last_modif": 1589123202.0, 
+   "size": 8613, 
+   "file_name": "filedir.html.js"
+  }, 
+  {
+   "path": "web/module/filedir", 
+   "last_modif": 1589123643.0, 
+   "size": 15648, 
+   "file_name": "filedir.js"
+  }, 
+  {
+   "path": "web/module/metacat", 
+   "last_modif": 1589118565.0, 
+   "size": 38, 
+   "file_name": "metacat.css"
+  }, 
+  {
+   "path": "web/module/metacat", 
+   "last_modif": 1589123485.0, 
+   "size": 2950, 
+   "file_name": "metacat.js"
+  }, 
+  {
+   "path": "web/module/metacat", 
+   "last_modif": 1589117916.0, 
+   "size": 3233, 
+   "file_name": "metacat.html.js"
+  }, 
+  {
+   "path": "web/module/last_modified_files", 
+   "last_modif": 1589026749.0, 
+   "size": 1352, 
+   "file_name": "last_modified_files.html.js"
+  }, 
+  {
+   "path": "web/module/last_modified_files", 
+   "last_modif": 1589026561.0, 
+   "size": 650, 
+   "file_name": "last_modified_files.css"
+  }, 
+  {
+   "path": "web/module/last_modified_files", 
+   "last_modif": 1589114006.0, 
+   "size": 2544, 
+   "file_name": "last_modified_files.js"
+  }, 
+  {
+   "path": "web/lib", 
+   "last_modif": 1589114084.0, 
+   "size": 6148, 
+   "file_name": ".DS_Store"
+  }, 
+  {
+   "path": "web/lib", 
+   "last_modif": 1574466084.0, 
+   "size": 10806, 
+   "file_name": "mustache.min.js"
+  }, 
+  {
+   "path": "web/lib/selectize", 
+   "last_modif": 1588975281.0, 
+   "size": 10946, 
+   "file_name": "selectize.bootstrap3.css"
+  }, 
+  {
+   "path": "web/lib/selectize", 
+   "last_modif": 1588974936.0, 
+   "size": 45833, 
+   "file_name": "selectize.min.js"
+  }, 
+  {
+   "path": "web/lib/selectize", 
+   "last_modif": 1588975094.0, 
+   "size": 11881, 
+   "file_name": "selectize.default.css"
+  }, 
+  {
+   "path": "web/lib/selectize", 
+   "last_modif": 1588974989.0, 
+   "size": 8784, 
+   "file_name": "selectize.css"
+  }, 
+  {
+   "path": "web/lib/timer", 
+   "last_modif": 1583796868.0, 
+   "size": 1483, 
+   "file_name": "Timer.js"
+  }, 
+  {
+   "path": "web/lib/jquery", 
+   "last_modif": 1588093028.0, 
+   "size": 253668, 
+   "file_name": "jquery-ui.min.js"
+  }, 
+  {
+   "path": "web/lib/jquery", 
+   "last_modif": 1588093058.0, 
+   "size": 31286, 
+   "file_name": "jquery-ui.min.css"
+  }, 
+  {
+   "path": "web/lib/jquery", 
+   "last_modif": 1588983702.0, 
+   "size": 8196, 
+   "file_name": ".DS_Store"
+  }, 
+  {
+   "path": "web/lib/jquery", 
+   "last_modif": 1588951589.0, 
+   "size": 729, 
+   "file_name": "autoresize.js"
+  }, 
+  {
+   "path": "web/lib/jquery", 
+   "last_modif": 1575391229.0, 
+   "size": 88144, 
+   "file_name": "jquery.min.js"
+  }, 
+  {
+   "path": "web/lib/jquery/images", 
+   "last_modif": 1588952760.0, 
+   "size": 246, 
+   "file_name": "ui-bg_glass_65_ffffff_1x400.png"
+  }, 
+  {
+   "path": "web/lib/readmore", 
+   "last_modif": 1584454036.0, 
+   "size": 21430, 
+   "file_name": "readmore.min.js"
+  }, 
+  {
+   "path": "web/media", 
+   "last_modif": 1589122897.0, 
+   "size": 8196, 
+   "file_name": ".DS_Store"
+  }, 
+  {
+   "path": "web/media/img", 
+   "last_modif": 1583093610.0, 
+   "size": 211525, 
+   "file_name": "filedir_icon.png"
+  }, 
+  {
+   "path": "web/media/img", 
+   "last_modif": 1589123077.0, 
+   "size": 35968, 
+   "file_name": "home_icon.png"
+  }, 
+  {
+   "path": "web/media/img", 
+   "last_modif": 1589117200.0, 
+   "size": 20243, 
+   "file_name": "catalog_icon.png"
+  }, 
+  {
+   "path": "web/media/img", 
+   "last_modif": 1533235798.0, 
+   "size": 19994, 
+   "file_name": "loading.gif"
+  }, 
+  {
+   "path": "web/media/img", 
+   "last_modif": 1583003371.0, 
+   "size": 61647, 
+   "file_name": "folder_icon.png"
+  }, 
+  {
+   "path": "web/media/img", 
+   "last_modif": 1583192613.0, 
+   "size": 567, 
+   "file_name": "file_little_icon.png"
+  }, 
+  {
+   "path": "web/media/img/file_icon", 
+   "last_modif": 1583014869.0, 
+   "size": 27421, 
+   "file_name": "md.png"
+  }, 
+  {
+   "path": "web/media/img/file_icon", 
+   "last_modif": 1583081605.0, 
+   "size": 34788, 
+   "file_name": "gif.png"
+  }, 
+  {
+   "path": "web/media/img/file_icon", 
+   "last_modif": 1583082192.0, 
+   "size": 4635, 
+   "file_name": "sas7bdat.png"
+  }, 
+  {
+   "path": "web/media/img/file_icon", 
+   "last_modif": 1583082192.0, 
+   "size": 4635, 
+   "file_name": "sas.png"
+  }, 
+  {
+   "path": "web/media/img/file_icon", 
+   "last_modif": 1583081930.0, 
+   "size": 4645, 
+   "file_name": "styl.png"
+  }, 
+  {
+   "path": "web/media/img/file_icon", 
+   "last_modif": 1583234496.0, 
+   "size": 121601, 
+   "file_name": "aif.png"
+  }, 
+  {
+   "path": "web/media/img/file_icon", 
+   "last_modif": 1583081700.0, 
+   "size": 35426, 
+   "file_name": "json.png"
+  }, 
+  {
+   "path": "web/media/img/file_icon", 
+   "last_modif": 1583081760.0, 
+   "size": 32841, 
+   "file_name": "wav.png"
+  }, 
+  {
+   "path": "web/media/img/file_icon", 
+   "last_modif": 1583234600.0, 
+   "size": 7406, 
+   "file_name": "zip.png"
+  }, 
+  {
+   "path": "web/media/img/file_icon", 
+   "last_modif": 1583081871.0, 
+   "size": 16705, 
+   "file_name": "scss.png"
+  }, 
+  {
+   "path": "web/media/img/file_icon", 
+   "last_modif": 1583082005.0, 
+   "size": 11824, 
+   "file_name": "sql.png"
+  }, 
+  {
+   "path": "web/media/img/file_icon", 
+   "last_modif": 1583082192.0, 
+   "size": 4635, 
+   "file_name": "sas7bcat.png"
+  }, 
+  {
+   "path": "web/media/img/file_icon", 
+   "last_modif": 1583005175.0, 
+   "size": 81814, 
+   "file_name": "py.png"
+  }, 
+  {
+   "path": "web/media/img/file_icon", 
+   "last_modif": 1583004640.0, 
+   "size": 9802, 
+   "file_name": "js.png"
+  }, 
+  {
+   "path": "web/media/img/file_icon", 
+   "last_modif": 1583950630.0, 
+   "size": 11872, 
+   "file_name": "xml.png"
+  }, 
+  {
+   "path": "web/media/img/file_icon", 
+   "last_modif": 1583006809.0, 
+   "size": 3738, 
+   "file_name": "docx.png"
+  }, 
+  {
+   "path": "web/media/img/file_icon", 
+   "last_modif": 1583014745.0, 
+   "size": 26029, 
+   "file_name": "txt.png"
+  }, 
+  {
+   "path": "web/media/img/file_icon", 
+   "last_modif": 1583006418.0, 
+   "size": 19305, 
+   "file_name": "png.png"
+  }, 
+  {
+   "path": "web/media/img/file_icon", 
+   "last_modif": 1583013834.0, 
+   "size": 31839, 
+   "file_name": "pptx.png"
+  }, 
+  {
+   "path": "web/media/img/file_icon", 
+   "last_modif": 1583005144.0, 
+   "size": 9152, 
+   "file_name": "html.png"
+  }, 
+  {
+   "path": "web/media/img/file_icon", 
+   "last_modif": 1583014087.0, 
+   "size": 11123, 
+   "file_name": "jpg.png"
+  }, 
+  {
+   "path": "web/media/img/file_icon", 
+   "last_modif": 1583020158.0, 
+   "size": 30368, 
+   "file_name": "mp3.png"
+  }, 
+  {
+   "path": "web/media/img/file_icon", 
+   "last_modif": 1583006746.0, 
+   "size": 7255, 
+   "file_name": "pdf.png"
+  }, 
+  {
+   "path": "web/media/img/file_icon", 
+   "last_modif": 1583014028.0, 
+   "size": 10127, 
+   "file_name": "ppt.png"
+  }, 
+  {
+   "path": "web/media/img/file_icon", 
+   "last_modif": 1583014283.0, 
+   "size": 15157, 
+   "file_name": "php.png"
+  }, 
+  {
+   "path": "web/media/img/file_icon", 
+   "last_modif": 1583005293.0, 
+   "size": 26098, 
+   "file_name": "css.png"
+  }, 
+  {
+   "path": "web/media/img/file_icon", 
+   "last_modif": 1583081673.0, 
+   "size": 15611, 
+   "file_name": "svg.png"
+  }, 
+  {
+   "path": "web/media/img/file_icon", 
+   "last_modif": 1583013950.0, 
+   "size": 30584, 
+   "file_name": "mp4.png"
+  }, 
+  {
+   "path": "web/media/img/file_icon", 
+   "last_modif": 1583006310.0, 
+   "size": 64553, 
+   "file_name": "xlsx.png"
+  }, 
+  {
+   "path": "web/media/img/file_icon", 
+   "last_modif": 1583013346.0, 
+   "size": 23743, 
+   "file_name": "csv.png"
+  }, 
+  {
+   "path": "web/media/img/file_icon", 
+   "last_modif": 1583014087.0, 
+   "size": 11123, 
+   "file_name": "jpeg.png"
+  }, 
+  {
+   "path": "data", 
+   "last_modif": 1589123941.0, 
+   "size": 6148, 
+   "file_name": ".DS_Store"
+  }, 
+  {
+   "path": "data/filedir", 
+   "last_modif": 1589124276.0, 
+   "size": 126, 
+   "file_name": "basic_data.json.js"
+  }, 
+  {
+   "path": "data/filedir", 
+   "last_modif": 1589124276.0, 
+   "size": 17926, 
+   "file_name": "data.json.js"
+  }
+ ], 
+ "main_data": {
+  "scan_timestamp": 1589124310.0, 
+  "root_path": "/Users/bassim/Documents/code/", 
+  "use_alias_on_file": "false", 
+  "alias": "filedir", 
+  "scan_duration": 0.0, 
+  "file_type_icons": [
+   "md", 
+   "gif", 
+   "sas7bdat", 
+   "sas", 
+   "styl", 
+   "aif", 
+   "json", 
+   "wav", 
+   "zip", 
+   "scss", 
+   "sql", 
+   "sas7bcat", 
+   "py", 
+   "js", 
+   "xml", 
+   "docx", 
+   "txt", 
+   "png", 
+   "pptx", 
+   "html", 
+   "jpg", 
+   "mp3", 
+   "pdf", 
+   "ppt", 
+   "php", 
+   "css", 
+   "svg", 
+   "mp4", 
+   "xlsx", 
+   "csv", 
+   "jpeg"
+  ], 
+  "folder_to_scan": "filedir"
+ }, 
+ "directories_data": {
+  "__root__": {
+   "nb_folder": 20, 
+   "nb_file": 89, 
+   "directory": "filedir", 
+   "path": "", 
+   "file_types": {
+    "md": {
+     "nb_file": 1, 
+     "size": 2126
+    }, 
+    "py": {
+     "nb_file": 1, 
+     "size": 5615
+    }, 
+    "js": {
+     "nb_file": 24, 
+     "size": 479800
+    }, 
+    "gif": {
+     "nb_file": 1, 
+     "size": 19994
+    }, 
+    "html": {
+     "nb_file": 1, 
+     "size": 2150
+    }, 
+    "__nothing__": {
+     "nb_file": 9, 
+     "size": 84415
+    }, 
+    "png": {
+     "nb_file": 37, 
+     "size": 1076051
+    }, 
+    "cs": {
+     "nb_file": 1, 
+     "size": 6278
+    }, 
+    "command": {
+     "nb_file": 1, 
+     "size": 87
+    }, 
+    "R": {
+     "nb_file": 1, 
+     "size": 3782
+    }, 
+    "txt": {
+     "nb_file": 1, 
+     "size": 81
+    }, 
+    "css": {
+     "nb_file": 11, 
+     "size": 74995
+    }
+   }, 
+   "size": 1755374
+  }, 
+  "web": {
+   "nb_folder": 15, 
+   "nb_file": 74, 
+   "directory": "web", 
+   "path": "", 
+   "file_types": {
+    "gif": {
+     "nb_file": 1, 
+     "size": 19994
+    }, 
+    "__nothing__": {
+     "nb_file": 4, 
+     "size": 32784
+    }, 
+    "png": {
+     "nb_file": 37, 
+     "size": 1076051
+    }, 
+    "css": {
+     "nb_file": 11, 
+     "size": 74995
+    }, 
+    "js": {
+     "nb_file": 21, 
+     "size": 461715
+    }
+   }, 
+   "size": 1665539
+  }, 
+  "web/module/last_modified_files": {
+   "nb_folder": 0, 
+   "nb_file": 3, 
+   "directory": "last_modified_files", 
+   "path": "web/module", 
+   "file_types": {
+    "css": {
+     "nb_file": 1, 
+     "size": 650
+    }, 
+    "js": {
+     "nb_file": 2, 
+     "size": 3896
+    }
+   }, 
+   "size": 4546
+  }, 
+  "web/module/filedir": {
+   "nb_folder": 0, 
+   "nb_file": 3, 
+   "directory": "filedir", 
+   "path": "web/module", 
+   "file_types": {
+    "css": {
+     "nb_file": 1, 
+     "size": 7847
+    }, 
+    "js": {
+     "nb_file": 2, 
+     "size": 24261
+    }
+   }, 
+   "size": 32108
+  }, 
+  "web/media": {
+   "nb_folder": 2, 
+   "nb_file": 38, 
+   "directory": "media", 
+   "path": "web", 
+   "file_types": {
+    "gif": {
+     "nb_file": 1, 
+     "size": 19994
+    }, 
+    "__nothing__": {
+     "nb_file": 1, 
+     "size": 8196
+    }, 
+    "png": {
+     "nb_file": 36, 
+     "size": 1075805
+    }
+   }, 
+   "size": 1103995
+  }, 
+  "web/module": {
+   "nb_folder": 5, 
+   "nb_file": 19, 
+   "directory": "module", 
+   "path": "web", 
+   "file_types": {
+    "css": {
+     "nb_file": 6, 
+     "size": 11259
+    }, 
+    "js": {
+     "nb_file": 13, 
+     "size": 38428
+    }
+   }, 
+   "size": 49687
+  }, 
+  "data": {
+   "nb_folder": 1, 
+   "nb_file": 3, 
+   "directory": "data", 
+   "path": "", 
+   "file_types": {
+    "__nothing__": {
+     "nb_file": 1, 
+     "size": 6148
+    }, 
+    "js": {
+     "nb_file": 2, 
+     "size": 18052
+    }
+   }, 
+   "size": 24200
+  }, 
+  "web/media/img": {
+   "nb_folder": 1, 
+   "nb_file": 37, 
+   "directory": "img", 
+   "path": "web/media", 
+   "file_types": {
+    "gif": {
+     "nb_file": 1, 
+     "size": 19994
+    }, 
+    "png": {
+     "nb_file": 36, 
+     "size": 1075805
+    }
+   }, 
+   "size": 1095799
+  }, 
+  "web/lib": {
+   "nb_folder": 5, 
+   "nb_file": 14, 
+   "directory": "lib", 
+   "path": "web", 
+   "file_types": {
+    "__nothing__": {
+     "nb_file": 2, 
+     "size": 14344
+    }, 
+    "png": {
+     "nb_file": 1, 
+     "size": 246
+    }, 
+    "css": {
+     "nb_file": 4, 
+     "size": 62897
+    }, 
+    "js": {
+     "nb_file": 7, 
+     "size": 422093
+    }
+   }, 
+   "size": 499580
+  }, 
+  "web/lib/jquery/images": {
+   "nb_folder": 0, 
+   "nb_file": 1, 
+   "directory": "images", 
+   "path": "web/lib/jquery", 
+   "file_types": {
+    "png": {
+     "nb_file": 1, 
+     "size": 246
+    }
+   }, 
+   "size": 246
+  }, 
+  "web/module/dark_mode": {
+   "nb_folder": 0, 
+   "nb_file": 4, 
+   "directory": "dark_mode", 
+   "path": "web/module", 
+   "file_types": {
+    "css": {
+     "nb_file": 2, 
+     "size": 2567
+    }, 
+    "js": {
+     "nb_file": 2, 
+     "size": 1039
+    }
+   }, 
+   "size": 3606
+  }, 
+  "web/module/metacat": {
+   "nb_folder": 0, 
+   "nb_file": 3, 
+   "directory": "metacat", 
+   "path": "web/module", 
+   "file_types": {
+    "css": {
+     "nb_file": 1, 
+     "size": 38
+    }, 
+    "js": {
+     "nb_file": 2, 
+     "size": 6183
+    }
+   }, 
+   "size": 6221
+  }, 
+  "web/lib/jquery": {
+   "nb_folder": 1, 
+   "nb_file": 6, 
+   "directory": "jquery", 
+   "path": "web/lib", 
+   "file_types": {
+    "__nothing__": {
+     "nb_file": 1, 
+     "size": 8196
+    }, 
+    "png": {
+     "nb_file": 1, 
+     "size": 246
+    }, 
+    "css": {
+     "nb_file": 1, 
+     "size": 31286
+    }, 
+    "js": {
+     "nb_file": 3, 
+     "size": 342541
+    }
+   }, 
+   "size": 382269
+  }, 
+  "web/lib/selectize": {
+   "nb_folder": 0, 
+   "nb_file": 4, 
+   "directory": "selectize", 
+   "path": "web/lib", 
+   "file_types": {
+    "css": {
+     "nb_file": 3, 
+     "size": 31611
+    }, 
+    "js": {
+     "nb_file": 1, 
+     "size": 45833
+    }
+   }, 
+   "size": 77444
+  }, 
+  "web/media/img/file_icon": {
+   "nb_folder": 0, 
+   "nb_file": 31, 
+   "directory": "file_icon", 
+   "path": "web/media/img", 
+   "file_types": {
+    "png": {
+     "nb_file": 31, 
+     "size": 745855
+    }
+   }, 
+   "size": 745855
+  }, 
+  "web/module/footer": {
+   "nb_folder": 0, 
+   "nb_file": 3, 
+   "directory": "footer", 
+   "path": "web/module", 
+   "file_types": {
+    "css": {
+     "nb_file": 1, 
+     "size": 157
+    }, 
+    "js": {
+     "nb_file": 2, 
+     "size": 497
+    }
+   }, 
+   "size": 654
+  }, 
+  "web/lib/timer": {
+   "nb_folder": 0, 
+   "nb_file": 1, 
+   "directory": "timer", 
+   "path": "web/lib", 
+   "file_types": {
+    "js": {
+     "nb_file": 1, 
+     "size": 1483
+    }
+   }, 
+   "size": 1483
+  }, 
+  ".git": {
+   "nb_folder": 0, 
+   "nb_file": 0, 
+   "directory": ".git", 
+   "path": "", 
+   "file_types": {}, 
+   "size": 0
+  }, 
+  "data/filedir": {
+   "nb_folder": 0, 
+   "nb_file": 2, 
+   "directory": "filedir", 
+   "path": "data", 
+   "file_types": {
+    "js": {
+     "nb_file": 2, 
+     "size": 18052
+    }
+   }, 
+   "size": 18052
+  }, 
+  "config": {
+   "nb_folder": 0, 
+   "nb_file": 1, 
+   "directory": "config", 
+   "path": "", 
+   "file_types": {
+    "txt": {
+     "nb_file": 1, 
+     "size": 81
+    }
+   }, 
+   "size": 81
+  }, 
+  "web/lib/readmore": {
+   "nb_folder": 0, 
+   "nb_file": 1, 
+   "directory": "readmore", 
+   "path": "web/lib", 
+   "file_types": {
+    "js": {
+     "nb_file": 1, 
+     "size": 21430
+    }
+   }, 
+   "size": 21430
+  }
+ }
+}
